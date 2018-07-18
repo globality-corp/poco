70c43468b0c03423f47ce4da1cf5e280a803a707
Jaime Oyarzun K <jaime.oyarzun@globality.com>
2018-07-18 14:40:35 -0700

    Use real env

--- example/src/index.js
+++ example/src/index.js
@@ -3,24 +3,10 @@ import Poco, { HistoryPlugin, IntlPlugin, ReduxPlugin } from '@globality/poco';
 import createApp from './app';
 
 
-const POCO_ENVIRONMENT = Object.freeze({
-    remoteConfigs: {
-        enabled: true,
-        origin: 'https://mariote.dev.globality.io',
-        paths: ['/configs/common.json', '/configs/mariote.json'],
-    },
-    config: {
-        auth0: {
-            clientId: '<auth0-client-id>',
-            domain: '<auth0-domain>',
-        },
-    }
-}); // this will be replaced by a webpack loader and .env files
-
 const MyApp = new Poco({
     appName: 'poco-example',
     createApp,
-    environment: POCO_ENVIRONMENT,
+    environment: JSON.parse(process.env.REACT_APP_ENVIRONMENT || '{}'),
 });
 
 MyApp.load((app) => {
